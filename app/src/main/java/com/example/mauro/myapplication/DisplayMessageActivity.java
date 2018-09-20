ba58842bc01d4a90455c16d851c2083bb3651571
Mauro Pedano <mauro.pedano@gmail.com>
2018-09-19 23:42:14 -0300

    The very first time I upload a chunk of code

---
+++ app/src/main/java/com/example/mauro/myapplication/DisplayMessageActivity.java
@@ -0,0 +1,26 @@
+package com.example.mauro.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String barcode = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+//        String product = intent.getStringExtra(MainActivity.PRODUCT);
+
+        TextView barcode_sent = findViewById(R.id.barcode_show);
+//        TextView product_name = findViewById(R.id.product_input);
+        barcode_sent.setText(barcode);
+//        product_name.setText(product);
+    }
+}
